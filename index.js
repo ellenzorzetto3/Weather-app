45df2c2d39220644e567d88b71bd7756679c4d0e
Ellen Zorzetto <ellen.zorzetto.it@gmail.com>
2024-04-26 23:11:24 -0300

    search engile with the API

--- index.js
+++ index.js
@@ -9,7 +9,28 @@ if (minutes < 10) {
   hour1.innerHTML = `${hours}:${minutes}`;
 }
 
-let apiKey = "b2a5adcct04b33178913oc335f405433";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function updateWeather(response) {
+  let tempAnswer = Math.round(response.data.temperature.current);
+  let degree = document.querySelector("#degrees");
+  let cityPlace = document.querySelector("#city1");
 
+  degree.innerHTML = `${tempAnswer}ºC`;
+  cityPlace.innerHTML = response.data.city;
+}
 
+function tempSearch(city) {
+  let apiKey = "o3bfb209f55e8951210f40e6476fat3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function searchInput(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search");
+
+  tempSearch(cityInput.value);
+}
+let searchForm = document.querySelector("#cityForm");
+searchForm.addEventListener("submit", searchInput);
+
+tempSearch("Brasília");
